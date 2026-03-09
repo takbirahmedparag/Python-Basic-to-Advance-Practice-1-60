40b7fd56574fd64584c2fb76cc3eee8e4497596d
Takbir Ahmed Parag <parag2305341807@diu.edu.bd>
2026-03-09 17:46:46 +0600

    Practice File

---
+++ Practice day 1 comments,print,user input,variables.py
@@ -0,0 +1,26 @@
+
+
+#------Uaer Input---------
+# name, age , country is the user define variables
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+country = input("enter your country: ")
+
+#-------Print the Output----------
+print("My name is " +name)
+print("I am ",age,"years old")
+print("I am from" +country)
+
+#----variabel declared-----
+name1 = "Takbir Ahmed Parag"
+age1 = 21
+country = "Bangladeng"
+
+#-------Print the Output-------------
+print("Our new student name is " +name1)
+print(name1+ "live in Dhaka," +country)
+print("He is currently",age1,"years old")
+print("At the age of ",age1,"he has started to learn Python")
+
+
